486dfdb745ae0c75a6e2f5e0046f63601a092dad
victortechnologypress-droid <victortechnologypress@gmail.com>
2026-03-24 16:06:29 +0200

    Update boot.asm

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -1,13 +1,13 @@
-; kernel/boot/boot.asm
-; Entry point-ul kernelului LoraOS
-; GRUB cauta header-ul Multiboot si sare la _start
+; ============================================================
+;  LoraOS v1 - Boot Entry Point
+;  Locație: kernel/boot/boot.asm
+; ============================================================
 
-; ============================================================
-;  Constante Multiboot (GRUB le recunoaste automat)
-; ============================================================
-MULTIBOOT_MAGIC     equ 0x1BADB002
-MULTIBOOT_FLAGS     equ 0x00000003   ; Bit0=aliniaza module, Bit1=info memorie
-MULTIBOOT_CHECKSUM  equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+MULTIBOOT_PAGE_ALIGN  equ  1 << 0
+MULTIBOOT_MEMORY_INFO equ  1 << 1
+MULTIBOOT_MAGIC       equ  0x1BADB002
+MULTIBOOT_FLAGS       equ  MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+MULTIBOOT_CHECKSUM    equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
 
 section .multiboot
 align 4
@@ -15,37 +15,26 @@ align 4
     dd MULTIBOOT_FLAGS
     dd MULTIBOOT_CHECKSUM
 
-; ============================================================
-;  Stack initial (16KB)
-; ============================================================
 section .bss
 align 16
 stack_bottom:
-    resb 16384          ; 16 KB de stack
+    resb 16384 ; 16 KB de stack
 stack_top:
 
-; ============================================================
-;  Entry point: GRUB sare aici
-; ============================================================
 section .text
 global _start
-extern kernel_main      ; Definit in kernel/kernel.c
+extern kernel_main
 
 _start:
-    ; 1. Seteaza stack-ul
+    ; Inițializăm stiva
     mov esp, stack_top
 
-    ; 2. Salveaza informatiile Multiboot inainte de orice altceva
-    ;    eax = magic number (0x2BADB002 daca vine de la GRUB)
-    ;    ebx = adresa structurii multiboot_info
-    push ebx            ; multiboot_info* ptr -> param 2
-    push eax            ; magic number       -> param 1
+    ; Trimitem parametrii Multiboot către kernel_main
+    push ebx ; Adresa structurii de info
+    push eax ; Magic number
 
-    ; 3. Sari la kernel-ul C
     call kernel_main
 
-    ; 4. Daca kernel_main returneaza (nu ar trebui), halt
-.hang:
-    cli                 ; Dezactiveaza intreruperile
-    hlt                 ; Opreste procesorul
-    jmp .hang           ; Loop de siguranta
+.halt:
+    hlt
+    jmp .halt
